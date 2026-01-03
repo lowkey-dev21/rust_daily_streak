7210e4a6d1d740604a865e1af8017e7a938fbdf7
Lowkey Dev <174181680+lowkey-dev21@users.noreply.github.com>
2026-01-03 23:28:15 +0100

    Day 3 of learning Rust : Add incremental compilation artifacts for tut_struct
    - Created new query-cache.bin file to store query results for incremental compilation.
    - Added work-products.bin to track work products generated during compilation.
    - Initialized s-heioqc2a7d-081zu3h.lock file for lock management in incremental builds.

--- tut_struct/src/main.rs
+++ tut_struct/src/main.rs
@@ -1,20 +1,30 @@
-#[derive(Debug)]
-struct  Rectangle {
-    width: u32,
-    height : u32
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        active: true,
+        username: String::from("lowkey"),
+        email: String::from("oyetolamuiz@gmail.com"),
+        sign_in_count: 5,
+    };
 
-  let rect1  = Rectangle { width: 89, height: 90 };
-   let result =  area(&rect1);
-   println!(" the result of the area is {result}");
-   println!("this is the struct values, just keeping the streak tradition {rect1:#?}");
+    user1.email = String::from("newemail@gmail.com");
+    build_user(String::from("myemail@gmail.com"), String::from("lowkey"));
+
+    let mut user2 = User{
+        email: String::from("new@gmail.com"),
+        ..user1
+    };
+
+   
+
 }
 
-
-fn area(rectangle : &Rectangle) -> u32{
- 
- rectangle.width * rectangle.height
+fn build_user (email: String, username: String) -> User {
+    User { active: true, username, email, sign_in_count: 5 }
 }
